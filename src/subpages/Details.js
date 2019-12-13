3b33202bb3275ba2f99b8541911e193d83208a7f
theresamorelli <theresa.l.morelli@gmail.com>
2019-12-13 15:57:17 -0800

    Refactor info into arrays rendered by mapping, add info, add styling

--- src/subpages/Details.js
+++ src/subpages/Details.js
@@ -18,19 +18,23 @@ function Details() {
 
   const features = [
     "Custom-built, unique concept pizza truck with a real wood fired oven and full commercial kitchen, featuring a large working space with lots of windows.",
-    "At the center of the working space is the oven, which evenly distributes heat and cooks food quickly at around 750 degrees F. With over 2,000 sq. inches of cooking space, it can handle up to three 13” pizzas or five 8” pizzas at a time.",
+    "At the center of the kitchen area is the oven, which evenly distributes heat and cooks food quickly at around 750°F. With over 2,000 sq. in. of cooking space, the oven can handle up to three 13” pizzas or five 8” pizzas at a time.",
     "The oven faces the serving window, allowing customers the experience of seeing the crackling fire and food cooking in real time.",
+    "A rear utility room contains easy access to the power system, extra storage space, and a toilet and hand wash sink, making it easy to operate anywhere.",
+    "We've used the oven for our thin crust Neopolitan-style pizzas (usually under 2 minutes cooking time). The oven's high heat gives the crust a light, perfectly crispy outside and caramelizes the cheese and toppings.",
+    "We've also used the oven for cooking up some pretty darn good meats, roasted veggies, crusty breads, and more. We're excited to help you discover just how amazing high heat cooking can be.",
   ]
 
   const featureStyle = {
     fontSize: "1.5em",
-    padding: "12px",
+    padding: "20px 12px",
   }
 
   return (
     <div id="details" className="container-fluid gold-bg section">
       <div className="container">
         <div className="flexbox">
+        <h1 className="text-center my-4">Available Now!</h1>
           <div className="row">
             {features.map((feature, i) => {
               return (
--- src/subpages/Team.js
+++ src/subpages/Team.js
@@ -17,25 +17,80 @@ const Details = props => {
     }
   `)
 
-let [showTruckInfo, toggle] = useState(false);
+  const truckInfos = [
+    { topic: 'Year', category: '2000' },
+    { topic: 'Make', category: 'GMC' },
+    { topic: 'Model', category: 'U-Haul' },
+    { topic: 'Mileage', category: '169,500' },
+    { topic: 'Gas or Diesel', category: 'Gas' },
+    { topic: 'Length', category: "34' total (24' interior)" },
+    { topic: 'Current DOT certification', category: 'Intrastate CO' },
+  ];
+
+  const baseInfos = [
+    { description: { text: 'Kitchen space measures 163 sq. ft (8\’3”x 20\’6”)' }},
+    { description: { text: 'Slide wall adds 32 sq. ft (10\’x38”)' }},
+    { description: {
+        textWithLink: (
+          <span>
+             Built-in <a href="https://chicagobrickoven.com/products/cbo-1000-diy-kit" target="_blank">Chicago Brick Oven CB-1000</a> wood fired oven
+          </span>
+                      )
+      },
+      subInfos: [
+        { description: 'Cooking surface of 2,000 sq. in. (53" x 39")' },
+        { description: 'Max temperature above 1000°F' },
+        { description: 'Use to prepare anything cooked on a grill or in a conventional oven' },
+        { description: 'Signature FlameRoll® generates high, even temperatures' },
+      ],
+    },
+  ];
+
+  const marginBottom = {
+    marginBottom: '25px',
+  }
+
+  const [showTruckInfo, toggleTruck] = useState(false);
+  const [showBasePkgInfo, toggleBasePkg] = useState(false);
 
   return (
     <div id="team" className="container-fluid orange-bg section">
       <div className="container">
         <div className="flexbox">
           <h1 className="text-center my-4">Details</h1>
-          <div>
-            <h2>Truck Info <span className="pointer" onClick={() => toggle(!showTruckInfo)}>{showTruckInfo ? '-' : '+'}</span></h2>
+          <div className="truckInfo">
+            <p>The wood fired oven provides a natural separation for the kitchen into areas for prep and building pizzas on event day. The front serve window wall slides out to add extra space for working and equipment. There are plenty of cabinets and racks for storage above along with storage under the oven.</p>
+            <ul>
+              {
+                baseInfos.map((info, i) => {
+                  const subInfos = info.subInfos && info.subInfos.length ? info.subInfos : null;
+                  return subInfos ? (
+                    <div>
+                      <li key={i}>{info.description.text || info.description.textWithLink}</li>
+                        <ul>
+                          {
+                            subInfos.map((subInfo, j) => (
+                              <li>{subInfo.description}</li>
+                            ))
+                          }
+                        </ul>
+                    </div>
+                  ) : (
+                    <li key={i}>{info.description.text || info.description.textWithLink}</li>
+                  )
+                })
+              }
+            </ul>
           </div>
-          <div id="truckInfoWrapper" className={"truckInfo" + (!showTruckInfo ? ' show' : null)}>
-            <div><strong>Year:</strong> 2000</div>
-            <div><strong>Make:</strong> GMC</div>
-            <div><strong>Model:</strong> U-Haul</div>
-            <div><strong>Mileage:</strong> 169,390</div>
-            <div><strong>Gas or diesel:</strong> Gas</div>
-            <div><strong>Length:</strong> 34' total</div>
-            <div><strong>Current DOT certification:</strong> Intrastate CO</div>
-          </div>
+          <h2>Truck Info <span className="pointer" onClick={() => toggleTruck(!showTruckInfo)}>{showTruckInfo ? '-' : '+'}</span></h2>
+          <ul className={"toggleShow" + (!showTruckInfo ? ' show' : null)}>
+            {
+              truckInfos.map((info, i) => (
+                <li key={i}><strong>{info.topic}:</strong> {info.category}</li>
+              ))
+            }
+          </ul>
+          <h2 style={marginBottom}>Truck Only Package <span className="pointer" onClick={() => toggleBasePkg(!showBasePkgInfo)}>{showBasePkgInfo ? '-' : '+'}</span></h2>
         </div>
       </div>
     </div>
