b5e1b1a636cdb1861246d282e696ccdd970c73d2
theresamorelli <theresa.l.morelli@gmail.com>
2019-12-20 14:39:27 -0800

    Set up pricing section general structure and styling

--- src/subpages/Details.js
+++ src/subpages/Details.js
@@ -21,13 +21,47 @@ function Details() {
     padding: "20px 12px",
   }
 
+  let price = 80000
+
   return (
-    <div id="details" className="container-fluid gold-bg section">
+    <div id="details" className="container-fluid lt-grey-bg section">
       <div className="container detailsSection">
-        <div className="flexbox">
-          <h1 className="text-center my-4">Available Now!</h1>
-          <div className="row">
-            {/* {features.map((feature, i) => {
+        <h1 className="text-center my-4">Pricing</h1>
+        <p>
+          Several buying options are available for the truck and equipment for a
+          fully customizable setup.
+        </p>
+        <h1 className="center">Base price: {price}</h1>
+        <br />
+        <div className="option selected">
+          <h2 className="">Just the Truck</h2>
+          <div className="price-summary">
+            <p className="">Truck, oven, and everything shown above</p>
+            <h4>$80,000</h4>
+          </div>
+        </div>
+        <div className="option not-selected">
+          <h2>Kitchen Support Equipment</h2>
+          <div className="price-summary">
+            <p>
+              Equipment and supplies for running a mobile wood fired pizza
+              business
+            </p>
+            <h4>$7,000</h4>
+          </div>
+        </div>
+        <div className="option not-selected">
+          <h2>Second Unit Trailer</h2>
+          <p>Mobile mini-kitchen that makes any setup more flexible</p>
+        </div>
+        <div className="option not-selected">
+          <h2>Business Turnkey</h2>
+          <p>
+            Training to make our delicious pizza plus marketing and logo
+            supplies
+          </p>
+        </div>
+        {/* {features.map((feature, i) => {
               return (
                 <div
                   className="feature"
@@ -38,8 +72,6 @@ function Details() {
                 </div>
               )
             })} */}
-          </div>
-        </div>
       </div>
     </div>
   )
