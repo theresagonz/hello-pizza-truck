5c39a8579e51a2cfce419ecf6e2574620cb375d6
theresamorelli <theresa.l.morelli@gmail.com>
2020-02-06 16:32:12 -0800

    Fix navbar toggle on scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,47 @@
 import React, { useState } from "react"
 import "../global.css"
-import Navbar from "../components/Navbar";
-import Home from "../sections/Home";
-import Info from "../sections/Info";
-import Pricing from "../sections/Pricing";
-import Contact from "../sections/Contact";
-import Footer from "../components/Footer";
+import Navbar from "../components/Navbar"
+import Home from "../sections/Home"
+import Info from "../sections/Info"
+import Pricing from "../sections/Pricing"
+import Contact from "../sections/Contact"
+import Footer from "../components/Footer"
 
 function Index() {
-  const navbar = document.getElementsByClassName("navbar")[0];
-  const hideNavbar = () => navbar.style.top = "-72px";
-  const showNavbar = () => navbar.style.top = "0";
-
-  const checkForShowingNavbar = () => {
-    let prevScrollPos = window.pageYOffset;
-    window.onscroll = () => {
-      const currScrollPos = window.pageYOffset;
-      const maxScroll = document.body.clientHeight - window.innerHeight;
-      const isScrollingUp = prevScrollPos > currScrollPos;
-      const isAtTop = currScrollPos === 0;
-      const isAtBottom = currScrollPos === maxScroll;
-
-      if (navbar) {
-        if (isScrollingUp || isAtTop) {
-          showNavbar();
-        } else {
-          hideNavbar();
-        }
-      }
-      prevScrollPos = currScrollPos;
+  const toggleNavbar = (command) => {
+    const navbar = document.getElementsByClassName("navbar")[0]
+    if (navbar) {
+      if (command === 'show') {
+        navbar.style.top = "0"
+      } else if (command === 'hide')
+        navbar.style.top = "-72px"
     }
   }
 
-  checkForShowingNavbar();
+  window.onscroll = () => {
+    const shouldShowNavbar = checkForShowingNavbar()
+    if (shouldShowNavbar && !isNavbarVisible) {
+      toggleNavbar('show')
+      setNavbarVisibility(true)
+    }
+    if (!shouldShowNavbar && isNavbarVisible) {
+      toggleNavbar('hide')
+      setNavbarVisibility(false)
+    }
+  }
+
+  const checkForShowingNavbar = () => {
+    const currScrollPos = window.pageYOffset
+    const isScrollingUp = prevScrollPos > currScrollPos
+    const isAtTop = currScrollPos === 0
+    const maxScroll = document.body.clientHeight - window.innerHeight
+    const isAtBottom = currScrollPos === maxScroll
+    updateScrollPos(currScrollPos)
+    return (isScrollingUp || isAtTop) && !isAtBottom
+  }
+
+  const [isNavbarVisible, setNavbarVisibility] = useState(true)
+  const [prevScrollPos, updateScrollPos] = useState(window.pageYOffset)
 
   return (
     <div className="page">
